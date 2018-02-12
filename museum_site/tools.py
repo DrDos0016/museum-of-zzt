993111c06bb009e7a87250b20e3fe2c07ba1b525
Dr. Dos <doctordos@gmail.com>
2018-02-11 19:25:32 -0800

    Actually update the database entry when saving screenshots!

--- museum_site/tools.py
+++ museum_site/tools.py
@@ -95,10 +95,13 @@ def set_screenshot(request, pk):
     if request.POST.get("save"):
         print("SAVING")
         src = SITE_ROOT + "/museum_site/static/data/temp.png"
-        dst =  SITE_ROOT + "/museum_site/static/images/screenshots/" + file.letter + "/" + file.filename[:-4]
+        dst =  SITE_ROOT + "/museum_site/static/images/screenshots/" + file.letter + "/" + file.filename[:-4] + ".png"
         print(src)
         print(dst)
         shutil.copyfile(src, dst)
+
+        file.screenshot = file.filename[:-4] + ".png"
+        file.save()
 
     if os.path.isfile(SITE_ROOT + "/museum_site/static/data/" + request.GET.get("file", "")):
         os.remove(SITE_ROOT + "/museum_site/static/data/" + request.GET["file"])
