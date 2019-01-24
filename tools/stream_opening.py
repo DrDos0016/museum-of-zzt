18b67f5067b1a14e6d57f482d42839d54cca9032
Dr. Dos <doctordos@gmail.com>
2019-01-23 21:44:22 -0800

    Templates for YouTube videos when generating stream info

--- tools/stream_opening.py
+++ tools/stream_opening.py
@@ -1,13 +1,37 @@
 import zookeeper
 
+YOUTUBE_DESC = """Livestream of the ZZT world "{title}" by {author} ({year})
+
+
+
+Download: https://museumofzzt.com/file/{letter}/{filename}
+Play online: https://museumofzzt.com/play/{letter}/{filename}
+
+Watch other live ZZT streams - https://twitch.tv/worldsofzzt
+====================
+
+Follow Worlds of ZZT on Twitter - https://twitter.com/worldsofzzt
+Check out the Museum of ZZT - https://museumofzzt.com
+Support Worlds of ZZT on Patreon - https://patreon.com/worldsofzzt
+"""
+
 def main():
     # 7,19 through 35,23. 29x5 workable area, x=21 is center)
     init_lines = []
-    init_lines.append(input("Line 1 (game title): "))
-    init_lines.append(input("Line 1 (game title 2): "))
-    init_lines.append(input("Line 3 (author): "))
-    init_lines.append(input("Line 4 (company): "))
-    init_lines.append("-" + input("Line 5 (year): ") + "-")
+    title1 = input("Line 1 (game title): ")
+    init_lines.append(title1)
+
+    title2 = input("Line 1 (game title 2): ")
+    init_lines.append(title2)
+
+    author = input("Line 3 (author): ")
+    init_lines.append(author)
+
+    company = input("Line 4 (company): ")
+    init_lines.append(company)
+
+    year = input("Line 5 (year): ")
+    init_lines.append("-" + year + "-")
 
     # Center the text
     lines = []
@@ -48,6 +72,26 @@ def main():
     z.save()
 
     b.screenshot("/mnt/ez/pictures/streaming/intro")
+
+    # Text overlay
+    with open("/mnt/ez/documents/wozzt/stream_text.txt", "w") as fh:
+        fh.write(title1 + " " + title2 + "\n")
+        fh.write("By: " + author + "\n")
+        fh.write(year)
+
+    # Youtube Description
+    with open("/mnt/ez/documents/wozzt/youtube_desc.txt", "w") as fh:
+        letter = input("Letter for play/view URLS: ").lower()
+        filename = input("Filename play/view URLS (with extension): ")
+        formatted = YOUTUBE_DESC.format(
+            title=(title1 + " " + title2).strip(),
+            author=author,
+            year=year,
+            letter=letter,
+            filename=filename
+        )
+        fh.write(formatted)
+
 
 
     return True
