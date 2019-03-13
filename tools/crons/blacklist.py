f589637870e346dbbc5f065c05b6d2f57b4c223d
Dr. Dos <doctordos@gmail.com>
2019-03-12 17:01:22 -0700

    Migrated Worlds of ZZT bot to use an external blacklist with support for board-specific blacklisted content. Removed old 1px transparency for Twitter images

---
+++ tools/crons/blacklist.py
@@ -0,0 +1,83 @@
+# Files blacklisted from Worlds of ZZT Bot's feed
+
+BLACKLISTED_FILES = [
+    951,  # G**g J*****n And STK Revealed
+    1779,  # 1**9 T*o
+    1577,  # 9**1: A Z*T M******l
+    1927,  # N**e E*****n
+    1811,  # B*N L***N!!
+    1852,  # M*****a R***s
+    2064,  # M*****a R***s SE
+    1496,  # B******! (Author's request)
+    2542,  # E**e K****e M****m-M***k! (Author's request)
+    493,  # G***D P**t 1! (Author's request)
+    494,  # G***D P**t 2! (Author's request)
+    494,  # G***D S********k! (Author's request)
+    2242,  # M*****'s Z*T T*******s (Author's request)
+    2242,  # P******l (Author's request)
+    1197,  # P******l B* (Author's request)
+    925,  # T*e P****e F***s I* (Author's request)
+    1723,  # T***h (Author's request)
+    1801,  # Z*TV 8 (Author's request TEMPORARY UNTIL BOARDS ARE HANDLED)
+    1901,  # Z*TV 9 (Author's request TEMPORARY UNTIL BOARDS ARE HANDLED)
+    2059,  #M****m B***s A*******y (Harassing)
+    2095,  #M****m B***s 3 (Harassing)
+    2540,  #M****m B***s 4 (Harassing)
+    2087,  #M****m B***s 1 (Harassing)
+    2084,  #M****m B***s 2 (Harassing)
+]
+
+
+BLACKLISTED_BOARDS = {
+    # Format: {File ID:ZZT Filename}: (Board #, Board #, ...)
+    "1801:zztv8-1.zzt": (0, 1, 2),
+    "1801:zztv8-2.zzt": (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19),
+    "1801:zztv8-3.zzt": (0, 1, 2, 12, 13, 14, 15, 16, 17),
+    "1801:zztv8-4.zzt": (0, 1, 2, 12, 13, 14, 15, 16, 17),
+    "1901:zztv9-1.zzt": (0, 1, 2, 51),
+    "1901:zztv9-2.zzt": (0, 1, 2),
+    "1901:zztv9-3.zzt": (0, 1, 2),
+    "1901:zztv9-4.zzt": (0, 1, 2),
+    "1901:zztv9-5.zzt": (0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32),
+}
+
+
+BOARD_TITLE_BLACKLIST = [
+    "!;MadTom's ColorKit", "!;MadTom's ZZToolKit", "!The NOC Color Kit", "$ **.**Tripping Color Kit**.**", "$- == (( ( paramach ) )) == -", "$ K Z T - Color Kit",
+    "$ ZZT Palette Plus v1.1", "$-( Blend Box )-", "$(( ( Paranoid Machinations ) ))", "$() Dude Scott This is Josh ()", "$({[/-=+Colors R' Us 2.25 +=-\]})",
+    "$(= SGP ColorKit v1.0 =)", "$)][( dexter's pepsi-kit )][(", "$*)|(* dexscam! *)|(*", "$*-*-* Mega Color Box '98 *-*-*", "$**GameMstr1's MegaShades Board**",
+    "$*-< CorPuS HeRmetIcUm >-*", "$*-=yllek's kewl color board=-*", "$:: hm's wonkeriffic toolkit ::", "$::<< BenKit >>::", "$:[ dexkit ]:", "$:{*pisces~iscariot*}  version 666",
+    "$:-BesTK-:", "$[[[ [  suburbiaTRiUMPHANT  ] ]]]", "$[] DarkMage ColorKit []", "$[=- textures by eJECTION13 -=]", "$[glt-kit by tboT]", "$[Jacob Hammond's Colorkit]",
+    "$_/\" VisuoFunctional Spectrum _/\"", "$_Underscore_", "$+*=-{[ULTIMA COLOR BOARD]}-=*+", "$-+=Flame, Inc. Color Kit=+-", "$-+-The-Shaft-tool-kit-+-", "$<(divided)>",
+    "$<> FutureWare Blending Kit <>", "$-= Raichu's Circuitbox v6.28 =-", "$-=([ <3\/|$|0/\/ ])=-", "$-=(MRFLOYDS COLOR BOX)=-",
+    "$-=* * * 3 Palette * * *=-", "$-=\EM's Damn ZZT-Kit/=-", "$-=|DirrCo Krazie Kitt 3|=-", "$-=+*PARASITE'S TOOL KIT*+=-",
+    "$--==::Noboyuki's ToolSet::==--", "$--==<<BenKit 2 (or BQKiT)>>==--", "$==--Magical Mystical ColorBox--==", "$-=Mondo SaxxonPike ToolBox=-",
+    "$-=SNACK 4.0 (board 1)=-", "$-=SNACK 4.0 (Board 2)=-", "$>>COLOR TOOLBOX<<", "$3d vFINAL - Josh Microwave", "$A.I.Z.I Advanced with ZOP", "$AKWare Tool-Kit",
+    "$AKWare Tool-Kit V2.0", "$ALL v1.0 for ZZT", "$barrel 'o fun kit", "$Bocco11's ZZToolkit v1.10", "$Creemy Chock n' Cheez Froot Lumps", "$DGE Color Kit",
+    "$DrCrab's Crammed Board", "$Dude Scott This is Josh V3.0", "$Ed's Colorkit", "$Embassy Games Color Center", "$Headache- Inducing- Evil.",
+    "$HGSI Coloring Kit (v4)", "$JM's All Purpose ZZT Toolbox", "$KPCK V. 1.0", "$MIG toolkit", "$Misteroo Rebellion/AKWare Toolkit", "$MNMZ Soft Toolkit v1.3",
+    "$Mystical Winds TK - Art Set Adv.", "$Mystickcal Color Toolkit", "$Newt's Toolbox", "$packboard.v1.aetsch.if.2000", "$playing this board will kill you",
+    "$Ra/\/|)()/\/\ I/\/C TK V. NUTS", "$ROOKIT 2", "$Shades Toolbox", "$SIMBA SEZ ROAR", "$Spectracolor", "$StreamLineSTK versionArr", "$Terrain Toolbox - Ice",
+    "$The KjKit!   Board 3", "$The KjKit!  Board 2", "$The KjKit! Board 1", "$The KjKit! Board 2", "$The KjKit! Board 3", "$The ZZT First Aid Kit", "$Ultra Colour",
+    "$Vecchio's Toolkit", "$vin3y $hades Toolbo><", "$VINEY80X", "$vstk - v. peter gabriel", "$wtog", "$ZZT First Aid Kit", "$ZZTek Color Board - by kev carter",
+    "$ZZTek Colour Kit", "::   - ZZT Rainbow v3.7 -   ::", ":: NMZmaster Toolkit v3.0", "::/\/omo(7) update!", "::z.nadir's palace of bullet::", ":; 2z + t",
+    ":; Compressed Textkit", ":; Friday Night Fever", ":; UseLESSS ReDUNDAncYYYYy", ":;.silverspectrum v.karma", ":;[ the draco toolkit ]", ":;_-=(EarthquakeTK)=-_",
+    ":;_-=+X(ETK 2)X+=-_", ":;-=( beef's ultra funky tool kit", ":;-=*Raptorian's Toolbox v2.1*=-", ":;Dave's ARG! PG Tool-kit", ":;dexmono vthe fly",
+    ":;lemmer toolkit: v100", ":;lemmer's ascii chart on fire", ":;Quantum's Toolkit (1)", ":;Quantum's Toolkit (3)", ":;rom v. hall of heads2k",
+    ":WinS Toolbox v2.5", "Quantum's Toolkit (2)", "Quantum's Toolkit (4)", "*NEW STK--Objects", "*NEW STK--Objects, Blinking", "*NEW STK--Boulders, Both", "*NEW STK--N/S Sliders, Both",
+    "*NEW STK--E/W Sliders, Both", "*NEW STK--Gems, Both", "*NEW STK--Keys, Both", "*NEW STK--Normal Walls, Both", "*NEW STK--Breakable Walls, Both", "*NEW STK--Invisible Walls, Both",
+    "*NEW STK--Fake Walls, Both", "*NEW STK--Water, Both", "*NEW STK--Bombs", "*NEW STK--Bombs, Blinking", "*NEW STK--Pushers", "*NEW STK--Pushers, Blinking", "*NEW STK--Spinning Guns",
+    "*NEW STK--Spinning Guns, Blinking", "*NEW STK--CounterClockwise", "*NEW STK--CounterCl., Blinking", "*NEW STK--Clockwise", "*NEW STK--Clockwise, Blinking", "*NEW STK--Transporters",
+    "*NEW STK--Transporters, Blinking", "*NEW STK--Blink Walls", "*NEW STK--Blink Walls, Blinking", "*NEW STK--Centipede Heads", "*NEW STK--Centip. Heads, Blinking",
+    "*NEW STK--Line Walls, Both", "*NEW STK--Solid Walls, Both", "*NEW STK--Pre Activated Bombs", "*NEW STK--Bullets", "*NEW STK--Ammo, Torches, more",
+    "*NEW STK--Doors, Passages, Floors", "*NEW STK--Ricochet", "*NEW STK--Strange Stuff", "*NEW STK--More Strange Stuff", "*MORESTK--Ammo, Both",
+    "*MORESTK--Torches, Both", "*MORESTK--Doors, Both", "*MORESTK--Star, Bl enemies, etc.", "*MORESTK--More Passages", "*MORESTK--Duplicators", "*MORESTK--Dupes, Blinking",
+    "*MORESTK--In-process Dupes", "*MORESTK--PreActivatedBombs1", "*MORESTK--PreActivatedBombs2", "*MORESTK--PreActivatedBombs3", "*MORESTK--Energizers, Both",
+    "*MORESTK--Forests, Both", "*MORESTK--Horiz. BWall Rays", "*MORESTK--All Bullets", "*MORESTK--Ricochets, Both", "*MORESTK--Text: B, G, Cy, R",
+    "*MORESTK--Text: P, Y, W", "*MORESTK--Still Bullets, Both", "*MORESTK--Empties, Both", "*WEIRDSTK--Crosses", "*WEIRDSTK--Passages",
+    "*WEIRDSTK--Carrots", "*WEIRDSTK--Dead pushers", "*WEIRDSTK--Smilies", "*WEIRDSTK--Dead bears", "*WEIRDSTK--Dead ruffians", "*WEIRDSTK--Dead lions",
+    "*WEIRDSTK--Dead tigers", "*WEIRDSTK--Dead heads", "*WEIRDSTK--Dead segments", "*WEIRDSTK--Breakables", "*WEIRDSTK--Breakables, blinking",
+    "*WIL STK--MONITORs w/o stats", "*WIL STK--Monitors", "*WIL STK--Monitors, Blinking", "*WIL STK--Slimes", "*WIL STK--Slimes, Blinking",
+    "*WIL STK--Slimes w/o stats", "*WIL STK--Hyper Enemies", "*WIL STK--More Hyper Enemies", "*WIL STK--Faster/Slower Bombs",
+    "*WIL STK--Scroll to Fakes", "*WIL STK--Scroll to Fakes, Blinkin"
+]
--- tools/crons/worlds_of_zzt.py
+++ tools/crons/worlds_of_zzt.py
@@ -16,7 +16,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museum.settings")
 django.setup()
 
 from museum_site.models import (
-    File, DETAIL_ZZT, DETAIL_SZZT, DETAIL_UPLOADED, DETAIL_GFX
+    File, DETAIL_ZZT, DETAIL_SZZT, DETAIL_UPLOADED, DETAIL_GFX, DETAIL_LOST
 )
 
 import zookeeper
@@ -27,6 +27,8 @@ from private import (
     WEBHOOK_URL
 )
 
+from blacklist import BLACKLISTED_FILES, BLACKLISTED_BOARDS, BOARD_TITLE_BLACKLIST
+
 
 def main():
     POST = True if "NOPOST" not in sys.argv else False
@@ -34,74 +36,6 @@ def main():
     ROOT = "/var/projects/museum"
     QUEUE = os.path.join(CRON_ROOT, "woz_queue.txt")
     APRIL = True if datetime.now().strftime("%m-%d") == "04-01" else False
-
-    BLACKLISTED_FILES = [
-        951,  # G**g J*****n And STK Revealed
-        1779,  # 1**9 T*o
-        1577,  # 9**1: A Z*T M******l
-        1927,  # N**e E*****n
-        1811,  # B*N L***N!!
-        1852,  # M*****a R***s
-        2064,  # M*****a R***s SE
-        1496,  # B******! (Author's request)
-        2542,  # E**e K****e M****m-M***k! (Author's request)
-        493,  # G***D P**t 1! (Author's request)
-        494,  # G***D P**t 2! (Author's request)
-        494,  # G***D S********k! (Author's request)
-        2242,  # M*****'s Z*T T*******s (Author's request)
-        2242,  # P******l (Author's request)
-        1197,  # P******l B* (Author's request)
-        925,  # T*e P****e F***s I* (Author's request)
-        1723,  # T***h (Author's request)
-        1801,  # Z*TV 8 (Author's request TEMPORARY UNTIL BOARDS ARE HANDLED)
-        1901,  # Z*TV 9 (Author's request TEMPORARY UNTIL BOARDS ARE HANDLED)
-        2059,  #M****m B***s A*******y (Harassing)
-        2095,  #M****m B***s 3 (Harassing)
-        2540,  #M****m B***s 4 (Harassing)
-        2087,  #M****m B***s 1 (Harassing)
-        2084,  #M****m B***s 2 (Harassing)
-
-    ]
-
-    BOARD_TITLE_BLACKLIST = [
-        "!;MadTom's ColorKit", "!;MadTom's ZZToolKit", "!The NOC Color Kit", "$ **.**Tripping Color Kit**.**", "$- == (( ( paramach ) )) == -", "$ K Z T - Color Kit",
-        "$ ZZT Palette Plus v1.1", "$-( Blend Box )-", "$(( ( Paranoid Machinations ) ))", "$() Dude Scott This is Josh ()", "$({[/-=+Colors R' Us 2.25 +=-\]})",
-        "$(= SGP ColorKit v1.0 =)", "$)][( dexter's pepsi-kit )][(", "$*)|(* dexscam! *)|(*", "$*-*-* Mega Color Box '98 *-*-*", "$**GameMstr1's MegaShades Board**",
-        "$*-< CorPuS HeRmetIcUm >-*", "$*-=yllek's kewl color board=-*", "$:: hm's wonkeriffic toolkit ::", "$::<< BenKit >>::", "$:[ dexkit ]:", "$:{*pisces~iscariot*}  version 666",
-        "$:-BesTK-:", "$[[[ [  suburbiaTRiUMPHANT  ] ]]]", "$[] DarkMage ColorKit []", "$[=- textures by eJECTION13 -=]", "$[glt-kit by tboT]", "$[Jacob Hammond's Colorkit]",
-        "$_/\" VisuoFunctional Spectrum _/\"", "$_Underscore_", "$+*=-{[ULTIMA COLOR BOARD]}-=*+", "$-+=Flame, Inc. Color Kit=+-", "$-+-The-Shaft-tool-kit-+-", "$<(divided)>",
-        "$<> FutureWare Blending Kit <>", "$-= Raichu's Circuitbox v6.28 =-", "$-=([ <3\/|$|0/\/ ])=-", "$-=(MRFLOYDS COLOR BOX)=-",
-        "$-=* * * 3 Palette * * *=-", "$-=\EM's Damn ZZT-Kit/=-", "$-=|DirrCo Krazie Kitt 3|=-", "$-=+*PARASITE'S TOOL KIT*+=-",
-        "$--==::Noboyuki's ToolSet::==--", "$--==<<BenKit 2 (or BQKiT)>>==--", "$==--Magical Mystical ColorBox--==", "$-=Mondo SaxxonPike ToolBox=-",
-        "$-=SNACK 4.0 (board 1)=-", "$-=SNACK 4.0 (Board 2)=-", "$>>COLOR TOOLBOX<<", "$3d vFINAL - Josh Microwave", "$A.I.Z.I Advanced with ZOP", "$AKWare Tool-Kit",
-        "$AKWare Tool-Kit V2.0", "$ALL v1.0 for ZZT", "$barrel 'o fun kit", "$Bocco11's ZZToolkit v1.10", "$Creemy Chock n' Cheez Froot Lumps", "$DGE Color Kit",
-        "$DrCrab's Crammed Board", "$Dude Scott This is Josh V3.0", "$Ed's Colorkit", "$Embassy Games Color Center", "$Headache- Inducing- Evil.",
-        "$HGSI Coloring Kit (v4)", "$JM's All Purpose ZZT Toolbox", "$KPCK V. 1.0", "$MIG toolkit", "$Misteroo Rebellion/AKWare Toolkit", "$MNMZ Soft Toolkit v1.3",
-        "$Mystical Winds TK - Art Set Adv.", "$Mystickcal Color Toolkit", "$Newt's Toolbox", "$packboard.v1.aetsch.if.2000", "$playing this board will kill you",
-        "$Ra/\/|)()/\/\ I/\/C TK V. NUTS", "$ROOKIT 2", "$Shades Toolbox", "$SIMBA SEZ ROAR", "$Spectracolor", "$StreamLineSTK versionArr", "$Terrain Toolbox - Ice",
-        "$The KjKit!   Board 3", "$The KjKit!  Board 2", "$The KjKit! Board 1", "$The KjKit! Board 2", "$The KjKit! Board 3", "$The ZZT First Aid Kit", "$Ultra Colour",
-        "$Vecchio's Toolkit", "$vin3y $hades Toolbo><", "$VINEY80X", "$vstk - v. peter gabriel", "$wtog", "$ZZT First Aid Kit", "$ZZTek Color Board - by kev carter",
-        "$ZZTek Colour Kit", "::   - ZZT Rainbow v3.7 -   ::", ":: NMZmaster Toolkit v3.0", "::/\/omo(7) update!", "::z.nadir's palace of bullet::", ":; 2z + t",
-        ":; Compressed Textkit", ":; Friday Night Fever", ":; UseLESSS ReDUNDAncYYYYy", ":;.silverspectrum v.karma", ":;[ the draco toolkit ]", ":;_-=(EarthquakeTK)=-_",
-        ":;_-=+X(ETK 2)X+=-_", ":;-=( beef's ultra funky tool kit", ":;-=*Raptorian's Toolbox v2.1*=-", ":;Dave's ARG! PG Tool-kit", ":;dexmono vthe fly",
-        ":;lemmer toolkit: v100", ":;lemmer's ascii chart on fire", ":;Quantum's Toolkit (1)", ":;Quantum's Toolkit (3)", ":;rom v. hall of heads2k",
-        ":WinS Toolbox v2.5", "Quantum's Toolkit (2)", "Quantum's Toolkit (4)", "*NEW STK--Objects", "*NEW STK--Objects, Blinking", "*NEW STK--Boulders, Both", "*NEW STK--N/S Sliders, Both",
-        "*NEW STK--E/W Sliders, Both", "*NEW STK--Gems, Both", "*NEW STK--Keys, Both", "*NEW STK--Normal Walls, Both", "*NEW STK--Breakable Walls, Both", "*NEW STK--Invisible Walls, Both",
-        "*NEW STK--Fake Walls, Both", "*NEW STK--Water, Both", "*NEW STK--Bombs", "*NEW STK--Bombs, Blinking", "*NEW STK--Pushers", "*NEW STK--Pushers, Blinking", "*NEW STK--Spinning Guns",
-        "*NEW STK--Spinning Guns, Blinking", "*NEW STK--CounterClockwise", "*NEW STK--CounterCl., Blinking", "*NEW STK--Clockwise", "*NEW STK--Clockwise, Blinking", "*NEW STK--Transporters",
-        "*NEW STK--Transporters, Blinking", "*NEW STK--Blink Walls", "*NEW STK--Blink Walls, Blinking", "*NEW STK--Centipede Heads", "*NEW STK--Centip. Heads, Blinking",
-        "*NEW STK--Line Walls, Both", "*NEW STK--Solid Walls, Both", "*NEW STK--Pre Activated Bombs", "*NEW STK--Bullets", "*NEW STK--Ammo, Torches, more",
-        "*NEW STK--Doors, Passages, Floors", "*NEW STK--Ricochet", "*NEW STK--Strange Stuff", "*NEW STK--More Strange Stuff", "*MORESTK--Ammo, Both",
-        "*MORESTK--Torches, Both", "*MORESTK--Doors, Both", "*MORESTK--Star, Bl enemies, etc.", "*MORESTK--More Passages", "*MORESTK--Duplicators", "*MORESTK--Dupes, Blinking",
-        "*MORESTK--In-process Dupes", "*MORESTK--PreActivatedBombs1", "*MORESTK--PreActivatedBombs2", "*MORESTK--PreActivatedBombs3", "*MORESTK--Energizers, Both",
-        "*MORESTK--Forests, Both", "*MORESTK--Horiz. BWall Rays", "*MORESTK--All Bullets", "*MORESTK--Ricochets, Both", "*MORESTK--Text: B, G, Cy, R",
-        "*MORESTK--Text: P, Y, W", "*MORESTK--Still Bullets, Both", "*MORESTK--Empties, Both", "*WEIRDSTK--Crosses", "*WEIRDSTK--Passages",
-        "*WEIRDSTK--Carrots", "*WEIRDSTK--Dead pushers", "*WEIRDSTK--Smilies", "*WEIRDSTK--Dead bears", "*WEIRDSTK--Dead ruffians", "*WEIRDSTK--Dead lions",
-        "*WEIRDSTK--Dead tigers", "*WEIRDSTK--Dead heads", "*WEIRDSTK--Dead segments", "*WEIRDSTK--Breakables", "*WEIRDSTK--Breakables, blinking",
-        "*WIL STK--MONITORs w/o stats", "*WIL STK--Monitors", "*WIL STK--Monitors, Blinking", "*WIL STK--Slimes", "*WIL STK--Slimes, Blinking",
-        "*WIL STK--Slimes w/o stats", "*WIL STK--Hyper Enemies", "*WIL STK--More Hyper Enemies", "*WIL STK--Faster/Slower Bombs",
-        "*WIL STK--Scroll to Fakes", "*WIL STK--Scroll to Fakes, Blinkin"
-    ]
 
     # Check if there's anything in the manual queue
     """ Queue Format: YYYY-MM-DD:HOUR:PK:FILENAME:BOARD
@@ -129,13 +63,14 @@ def main():
         line = line.strip()
         print("LINE", line)
 
-        date, hour, pk, filename, board = line.split(":")
-        # Do we use this?
-        if (source == "RANDOM") and ((date == "*" or date == current_date) and (hour == "*" or hour == current_hour)):
-            source = "QUEUE"
-            queue_data = {"pk":int(pk), "filename":filename, "board":int(board)}
-        else: # if we don't use it, add it to the output text file
-            output += line + "\n"
+        if line.strip() != "":
+            date, hour, pk, filename, board = line.split(":")
+            # Do we use this?
+            if (source == "RANDOM") and ((date == "*" or date == current_date) and (hour == "*" or hour == current_hour)):
+                source = "QUEUE"
+                queue_data = {"pk":int(pk), "filename":filename, "board":int(board)}
+            else: # if we don't use it, add it to the output text file
+                output += line + "\n"
     print("---")
 
     # Write the new queue
@@ -150,7 +85,7 @@ def main():
         # Select a random zip
         # - Zip must have a ZZT World in it and be published
         # - Zip must not have modified graphics
-        qs = File.objects.filter(details__in=[DETAIL_ZZT]).exclude(details__in=[DETAIL_UPLOADED, DETAIL_GFX]).order_by("?")
+        qs = File.objects.filter(details__in=[DETAIL_ZZT]).exclude(details__in=[DETAIL_UPLOADED, DETAIL_GFX, DETAIL_LOST]).order_by("?")
 
         # Filter out blacklisted files
         for file_obj in qs:
@@ -194,18 +129,30 @@ def main():
         # Select a random board (unless it's Tuesday)
         processing = True
         board_name = ""
+        bl_key = str(data.id) + ":" + selected  # Blacklist key
+        bl = BLACKLISTED_BOARDS.get(bl_key, [])
+
         while processing:
             if tuesday:
                 board_num = 0
                 processing = False
-            else:
+                if (0 in BLACKLISTED_BOARDS.get(bl_key, [])):
+                    # In case the title screen is blacklisted and it's tuesday
+                    tuesday = False
+                    processing = True
+
+            if not tuesday:
                 board_num = random.randint(0, len(z.boards) - 1)
 
             # Verify the board isn't a common toolkit
             if processing and z.boards[board_num].title in BOARD_TITLE_BLACKLIST:
                 continue
 
-            z.boards[board_num].screenshot(CRON_ROOT + "temp", title_screen=(board_num == 0), format="RGBA")
+            # Verify the board isn't in the board blacklist
+            if processing and board_num in bl:
+                continue
+
+            z.boards[board_num].screenshot(CRON_ROOT + "temp", title_screen=(board_num == 0))
             board_name = z.boards[board_num].title
             processing = False
     elif source == "QUEUE":
@@ -226,7 +173,7 @@ def main():
         z = zookeeper.Zookeeper(os.path.join(CRON_ROOT, queue_data["filename"]))
 
         # Render
-        z.boards[queue_data["board"]].screenshot(CRON_ROOT + "temp", title_screen=(queue_data["board"] == 0), format="RGBA", dark=APRIL)
+        z.boards[queue_data["board"]].screenshot(CRON_ROOT + "temp", title_screen=(queue_data["board"] == 0), dark=APRIL)
         board_name = z.boards[queue_data["board"]].title
 
     # Remove the ZZT file. We're done with it.
@@ -317,17 +264,11 @@ def main():
         print(twitter_post)
         print("Posting to twitter...")
 
-        # Fix the image
-        orig = Image.open(CRON_ROOT + "temp.png")
-        temp = orig.load()
-        temp[0,0] = (temp[0,0][0], temp[0,0][1], temp[0,0][2], 254)
-        orig.save(CRON_ROOT + "twitter.png")
-
         # April
         if APRIL:
             twitter_post = "Room is dark - you need to light a torch!\n" + twitter_post
 
-        with open(CRON_ROOT + "twitter.png", "rb") as imagefile:
+        with open(CRON_ROOT + "temp.png", "rb") as imagefile:
             imagedata = imagefile.read()
 
             t_up = Twitter(domain='upload.twitter.com', auth=OAuth(TWITTER_OAUTH_TOKEN, TWITTER_OAUTH_SECRET, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET))
