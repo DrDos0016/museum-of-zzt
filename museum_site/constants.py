80bfedff33df2bc4a7d619d59593850cf396dd80
Dr. Dos <doctordos@gmail.com>
2019-11-04 17:18:06 -0800

    Placeholder variables for when private file is missing

--- museum_site/constants.py
+++ museum_site/constants.py
@@ -1,4 +1,9 @@
-from museum_site.patron_secrets import PASSWORD2DOLLARS, PASSWORD5DOLLARS
+try:
+    from museum_site.patron_secrets import PASSWORD2DOLLARS, PASSWORD5DOLLARS
+except ModuleNotFoundError:
+    print("PATRON_SECRETS.PY NOT FOUND. USING DEV VALUES")
+    PASSWORD2DOLLARS = "test2dollars"
+    PASSWORD5DOLLARS = "test5dollars"
 
 # Article publish states
 PUBLISHED_ARTICLE = 1
