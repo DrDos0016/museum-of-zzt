6db1652414ecb7ed9a4181b33ab71af113aaac62
Dr. Dos <doctordos@gmail.com>
2020-04-06 18:25:26 -0700

    Only check archive.org links if a link has been set

--- tools/archive_link_test.py
+++ tools/archive_link_test.py
@@ -12,10 +12,15 @@ django.setup()
 from museum_site.models import File
 
 def main():
+    print("Checks all files for valid archive.org links")
+    input("Press enter to continue")
     for f in File.objects.all().order_by("id"):
-        resp = requests.get("https://archive.org/embed/" + f.archive_name)
-        if resp.status_code != 200:
-            print(resp.status_code, f.id, f.title)
+        if f.archive_name:
+            resp = requests.get("https://archive.org/embed/" + f.archive_name)
+            if resp.status_code != 200:
+                print(resp.status_code, f.id, f.title)
+        else:
+            print(f, "has no value for archive_name")
     return True
 
 if __name__ == "__main__":
