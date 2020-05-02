a33264c82d8940d1832f2bf692510707fe0b7124
Dr. Dos <doctordos@gmail.com>
2020-05-02 16:16:16 -0700

    Remove hardcoded paths

--- tools/dump_database.py
+++ tools/dump_database.py
@@ -3,7 +3,7 @@ import sys
 
 import django
 
-sys.path.append("/var/projects/museum/")
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museum.settings")
 django.setup()
 
