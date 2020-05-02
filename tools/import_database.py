da9157725937e8f9495f65c4cd1d940bbac967b6
Dr. Dos <doctordos@gmail.com>
2020-05-02 15:49:20 -0700

    Tool to import database dumps and tool to scrub removed articles/files from the database.

---
+++ tools/import_database.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+import django
+
+sys.path.append("/var/projects/museum/")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museum.settings")
+django.setup()
+
+from museum.private import DATABASES  # noqa: E402
+
+
+def main():
+    user = DATABASES["default"]["USER"]
+    password = DATABASES["default"]["PASSWORD"]
+    name = DATABASES["default"]["NAME"]
+    filename = "museum_db_dump.sql" if len(sys.argv) < 2 else sys.argv[-1]
+
+    print("WARNING! THIS WILL DESTROY THE EXISTING DATABASE")
+    print("Are you sure you wish to import {}?".format(filename))
+    confirm = input("Type 'yes' to confirm: ")
+
+    if confirm == "yes":
+
+        print("Importing {}...".format(name, filename))
+        command = "mysql -u {} -p{} {} < {}".format(
+            user, password, name, filename
+        )
+        os.system(command)
+        print("DONE.")
+    else:
+        print("ABORTED.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ tools/scrub_database.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+import django
+
+sys.path.append("/var/projects/museum/")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museum.settings")
+django.setup()
+
+from museum_site.models import *  # noqa: E402
+from museum_site.constants import REMOVED_ARTICLE, DETAIL_REMOVED  # noqa: E402
+
+
+def main():
+    print("WARNING! THIS WILL PERMANENTLY REMOVE DATA FROM THIS DATABASE")
+    print("Are you sure you wish to remove all non-public data?")
+    confirm = input("Type 'yes' to confirm: ")
+
+    if confirm == "yes":
+        print("Deleting articles...")
+        for a in Article.objects.filter(published=REMOVED_ARTICLE):
+            print(a)
+            a.delete()
+        print("Done!")
+        print("Deleting file objects...")
+        for f in File.objects.filter(details__id=DETAIL_REMOVED):
+            print(f)
+            f.delete()
+        print("Done!")
+        print("Private data has removed. Database can be publicly shared.")
+        print("DONE.")
+    else:
+        print("ABORTED.")
+
+
+if __name__ == '__main__':
+    main()
