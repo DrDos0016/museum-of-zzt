0b45ec48955f73a0e88422660af82ff6fd89333b
Dr. Dos <doctordos@gmail.com>
2020-07-21 09:39:58 -0700

    Use Tuesday branch on Tuesdays

--- tools/crons/newwoz.py
+++ tools/crons/newwoz.py
@@ -2,21 +2,27 @@ import os
 
 import django
 
+from datetime import datetime
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museum.settings")
 django.setup()
-
-print("NEW WOZZT TEST")
 
 from museum_site.wozzt_queue import WoZZT_Queue  # noqa: E402
 
 
 def main():
-    entry = WoZZT_Queue.objects.all().order_by("-priority", "id")[0]
+    now = datetime.now()
 
+    if now.weekday() == 1:  # Tuesday
+        entry = WoZZT_Queue.objects.filter(category="tuesday")
+    else:
+        entry = WoZZT_Queue.objects.all(category="wozzt")
+
+    entry = entry.order_by("-priority", "id")[0]
     entry.send_tweet()
     entry.delete_image()
     entry.delete()
-    print("Well that was easy.")
+    print("Done.")
 
 
 if __name__ == "__main__":
