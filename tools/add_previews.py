d113fd75456c14f651cb9769d922d9394b369d63
Dr. Dos <doctordos@gmail.com>
2020-09-23 09:53:23 -0700

    Add script explanation and message when script is complete.

--- tools/add_previews.py
+++ tools/add_previews.py
@@ -7,13 +7,22 @@ sys.path.append("/var/projects/museum/")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museum.settings")
 django.setup()
 
-from museum_site.models import *
-from museum_site.common import *
-from museum_site.constants import *
+from museum_site.models import *  # noqa: E402
+from museum_site.common import *  # noqa: E402
+from museum_site.constants import *  # noqa: E402
+
+HELP = """This script will list all articles without a preview image.
+If a PNG matched the article's PK is in static/images/articles/previews it
+will automatically be assigned.
+
+Press ENTER to begin."""
 
 
 def main():
+    input(HELP)
+
     articles = Article.objects.filter(preview="").order_by("id")
+
     for a in articles:
         path = os.path.join(
             SITE_ROOT, "museum_site/static/images/articles/previews/{}.png"
@@ -26,6 +35,7 @@ def main():
         else:
             print("[ ]", a.id, a.title)
 
+    print("Done.")
     return True
 
 
