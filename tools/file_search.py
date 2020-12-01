e58390be779ac9a33697b4937f8eee878a6a3d41
Dr. Dos <doctordos@gmail.com>
2020-12-01 15:04:52 -0800

    Fix numerous issues

--- tools/file_search.py
+++ tools/file_search.py
@@ -8,74 +8,72 @@ sys.path.append("/var/projects/museum")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museum.settings")
 django.setup()
 
-from museum_site.models import File
+from museum_site.models import File  # noqa: E402
+from museum_site.constants import SITE_ROOT  # noqa: E402
 
 
 def main():
     shortcuts = {
-        "1":"(.*).[zZ][zZ][tT]$",
-        "2":"(.*).[sS][zZ][tT]$",
-        "3":"(.*).[zZ][iI][gG]$",
-        "4":"(.*).[zZ][zZ][mM]$",
-        "5":"(.*).[tT][xX][tT]$",
-        "6":"(.*).[dD][oO][cC]$",
-        "7":"(.*).[bB][rR][dD]$",
-        "8":"(.*).[eE][xX][eE]$",
+        "1": "(.*).[zZ][zZ][tT]$",
+        "2": "(.*).[sS][zZ][tT]$",
+        "3": "(.*).[zZ][iI][gG]$",
+        "4": "(.*).[zZ][zZ][mM]$",
+        "5": "(.*).[tT][xX][tT]$",
+        "6": "(.*).[dD][oO][cC]$",
+        "7": "(.*).[bB][rR][dD]$",
+        "8": "(.*).[eE][xX][eE]$",
+        "9": "(.*).[cC][oO][mM]$",
     }
+    length = len(list(shortcuts.keys()))
     print("SHORTCUTS")
-    for x in range(1,9):
+    for x in range(1, length + 1):
         print(x, shortcuts[str(x)])
 
     contains_regex = input("Regex to list Files that do match: ")
-    #contains_regex="1"
     if contains_regex in shortcuts.keys():
         contains_regex = shortcuts[contains_regex]
 
     lacks_regex = input("Regex to list Files that don't match: ")
-    #lacks_regex=""
     if lacks_regex in list(shortcuts.keys()):
         lacks_regex = shortcuts[lacks_regex]
 
     files = File.objects.all().order_by("letter", "title")
     lacks_matches = ""
     contains_matches = ""
+
+    print("LACKS", lacks_regex)
+
     for f in files:
         letter = f.letter
-        zip = f.filename
+        fn = f.filename
 
         # Open the zip
         try:
-            zip = zipfile.ZipFile(
-                "/var/projects/museum/zgames/" + letter + "/" + zip
+            zf = zipfile.ZipFile(
+                os.path.join(SITE_ROOT, "zgames", (letter + "/" + fn))
             )
         except Exception as e:
             # The audit script should handle missing/invalid zips, not this.
+            print(e)
             continue
 
         # Print files
         lack_fail = False
         try:
-            file_list = zip.namelist()
+            file_list = zf.namelist()
 
-            for file in file_list:
+            for zfn in file_list:
                 if contains_regex:
-                    if (re.match(contains_regex, file)):
-                        contains_matches += str(f.id)+","
-                        #print("+", f.id, f.title)
-                        print(file, f.filename)
+                    if (re.match(contains_regex, zfn)):
+                        print(fn, zfn)
                 if lacks_regex:
-                    if (re.match(contains_regex, file)):
-                        lack_fail = True
-        except:
+                    if (not re.match(lacks_regex, zfn)):
+                        print(fn, zfn)
+        except Exception as e:
+            print(e)
             continue
-
-        if lacks_regex and lack_fail:
-            lack_matches += str(f.id)+","
-            print("-", f.id, f.title)
-
-    #print(contains_matches)
-    #print(lacks_matches)
     return True
+
 
 if __name__ == "__main__":
     main()
