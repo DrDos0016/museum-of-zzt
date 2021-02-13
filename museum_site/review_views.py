fc28ada9df526c2f4ce9211ad87b3586fd701056
Dr. Dos <doctordos@gmail.com>
2021-02-13 09:03:57 -0800

    Fix broken Paginator

--- museum_site/review_views.py
+++ museum_site/review_views.py
@@ -22,9 +22,16 @@ def review_directory(request, page_num=1):
     elif request.GET.get("sort") == "rating":
         qs = qs.order_by("-rating")
 
+
+    data["available_views"] = ["detailed", "list", "gallery"]
+    data["view"] = get_selected_view_format(request, data["available_views"])
+
     page_number = int(request.GET.get("page", 1))
-    start = (page_number - 1) * PAGE_SIZE
-    data["paginator"] = Paginator(qs, PAGE_SIZE)
+    page_size = get_page_size(data["view"])
+    start = (page_number - 1) * page_size
+    data["paginator"] = Paginator(qs, page_size)
+    if page_number > data["paginator"].num_pages:
+        page_number = 1
     data["page"] = data["paginator"].get_page(page_number)
     data["page_number"] = page_number
 
@@ -34,5 +41,7 @@ def review_directory(request, page_num=1):
         {"text": "Reviewer", "val": "reviewer"},
         {"text": "Rating", "val": "rating"},
     ]
+
+    print("WHAT")
 
     return render(request, "museum_site/review_directory.html", data)
