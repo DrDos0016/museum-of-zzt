02de1e45d937047d7fdbd2dd2a3bc5bbe7d12f39
Dr. Dos <doctordos@gmail.com>
2021-08-11 15:05:59 -0700

    Basic URL tester

--- museum_site/tests.py
+++ museum_site/tests.py
@@ -38,12 +38,28 @@ class URLStatusTest(TestCase):
             elif str(p.pattern) == "zeta-launcher/":
                 p.pattern = "zeta-live/?pk=1015&world=DEMO.ZZT&start=7"
             url = HOST + str(p.pattern)
+
+            # Replace args
+            url = url.replace("<int:article_id>", "2")
+            url = url.replace("<int:page>", "1")
+            url = url.replace("<slug:slug>", "url-status-test-slug")
+            url = url.replace("<slug:category>", "closer-look")
+            url = url.replace("<str:letter>", "z")
+            url = url.replace("<str:filename>", "zzt.zip")
+            url = url.replace("<int:pk>", "1015")
+            url = url.replace("<int:user_id>", "1")
+            url = url.replace("<str:unused_slug>", "account-name")
+
+            # More oddball features that will likely be cut
+            url = url.replace("<int:phase>", "1")
+            url = url.replace("<str:section>", "play")
+
+
             r = requests.head(url)
 
             if r.status_code >= 400:
                 print(r.status_code, url, "[{}]".format(p.pattern))
             if r.status_code == 500:
-                print("HEY THIS IS A 500!")
                 print(url)
 
             statuses[r.status_code] = True
@@ -56,3 +72,30 @@ class URLStatusTest(TestCase):
         print(unique_statuses)
 
         self.assertEqual(unique_statuses, expected_statuses)
+
+
+class FileviewerStatusTest(TestCase):
+    def test_file_viewer_url_status(self):
+        print("ABC")
+        qs = File.objects.all()
+        for f in qs:
+            print(f)
+
+
+        self.assertEqual(1 + 1, 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
