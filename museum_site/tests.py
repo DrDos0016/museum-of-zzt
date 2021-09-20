c5339154f3983e8a389f74fd2abeca3853fcb95d
Dr. Dos <doctordos@gmail.com>
2021-09-19 17:57:01 -0700

    Some tests!

--- museum_site/tests.py
+++ museum_site/tests.py
@@ -4,6 +4,7 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import unittest
 
 import requests
 
@@ -12,24 +13,17 @@ from django.test import TestCase
 from museum_site.common import *
 from museum_site.constants import *
 from museum_site.models import *
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from museum_site.templatetags.site_tags import *
 
 
 class URLStatusTest(TestCase):
+    @unittest.skip("Skipping test_url_status()")
     def test_url_status(self):
         from museum_site.urls import urlpatterns
-        statuses = {}
+        statuses = {200: True, 301: True, 302: True}  # Valid statuses
         expected_statuses = [200, 301, 302]
 
         for p in urlpatterns:
-
             # Special patterns
             if str(p.pattern) == ".":
                 p.pattern = ""
@@ -54,19 +48,13 @@ class URLStatusTest(TestCase):
             url = url.replace("<int:phase>", "1")
             url = url.replace("<str:section>", "play")
 
-
             r = requests.head(url)
 
             if r.status_code >= 400:
                 print(r.status_code, url, "[{}]".format(p.pattern))
-            if r.status_code == 500:
-                print(url)
 
             statuses[r.status_code] = True
 
-
-        print("Host", HOST)
-        print("Env", ENV)
         unique_statuses = list(statuses.keys())
         unique_statuses.sort()
         print(unique_statuses)
@@ -76,26 +64,52 @@ class URLStatusTest(TestCase):
 
 class FileviewerStatusTest(TestCase):
     def test_file_viewer_url_status(self):
-        print("ABC")
         qs = File.objects.all()
         for f in qs:
             print(f)
 
-
         self.assertEqual(1 + 1, 2)
 
 
+class MetaTagTest(TestCase):
+    def test_index(self):
+        valid = {}
+        expected = {"og:image": 1, "og:url": 1, "og:title": 1}
 
+        tags = meta_tags(path="/").split("\n")
+        for tag in tags:
+            if tag == (
+                '<meta property="og:image" '
+                'content="{}static/images/og_default.jpg">'.format(HOST)
+            ):
+                valid["og:image"] = 1
+            elif tag == '<meta property="og:url" content="{}">'.format(HOST):
+                valid["og:url"] = 1
+            elif tag == '<meta property="og:title" content="Museum of ZZT">':
+                valid["og:title"] = 1
+        self.assertEqual(valid, expected)
 
+    def test_default(self):
+        valid = {}
+        expected = {"og:image": 1, "og:url": 1, "og:title": 1}
 
+        tags = meta_tags(path="/credits/").split("\n")
+        for tag in tags:
+            if tag == (
+                '<meta property="og:image" '
+                'content="{}static/images/og_default.jpg">'.format(HOST)
+            ):
+                valid["og:image"] = 1
+            elif tag == '<meta property="og:url" content="{}credits/">'.format(HOST):
+                valid["og:url"] = 1
+            elif tag == '<meta property="og:title" content="Museum of ZZT">':
+                valid["og:title"] = 1
+        self.assertEqual(valid, expected)
 
-
-
-
-
-
-
-
-
-
-
+"""
+http://django.pi:8000/article/f/frost1.zip
+http://django.pi:8000/article/406/livestream-frost-1-power
+http://django.pi:8000/file/f/frost1.zip
+http://django.pi:8000/patron-articles/
+http://django.pi:8000/user/profile/1/dr_dos/
+"""
