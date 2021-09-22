d88cb7e63bb6b0ef03b3a2b0388a4ec05d39a057
Dr. Dos <doctordos@gmail.com>
2021-09-21 17:23:20 -0700

    add pep exception

--- tools/_blank-tool.py
+++ tools/_blank-tool.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museum.settings")
 django.setup()
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User  # noqa: E402
 
 from museum_site.models import *  # noqa: E402
 
