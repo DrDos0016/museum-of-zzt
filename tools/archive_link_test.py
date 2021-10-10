053489a828a77f8f931c08033e35ac8ca96aa103
Dr. Dos <doctordos@gmail.com>
2021-10-09 19:33:52 -0700

    Rewrite to modern standards. Use HEAD instead of GET.

--- tools/archive_link_test.py
+++ tools/archive_link_test.py
@@ -5,24 +5,21 @@ import requests
 
 import django
 
-sys.path.append("/var/projects/museum/")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "museum.settings")
 django.setup()
 
-from museum_site.models import File
+from museum_site.models import File  # noqa: E402
+
 
 def main():
     print("Checks all files for valid archive.org links")
     input("Press enter to continue")
     for f in File.objects.all().order_by("id"):
         if f.archive_name:
-            resp = requests.get("https://archive.org/embed/" + f.archive_name)
+            resp = requests.head("https://archive.org/embed/" + f.archive_name)
             if resp.status_code != 200:
-                print(resp.status_code, f.id, f.title)
-        else:
-            print(f, "has no value for archive_name")
+                print("{} #{} `{}`".format(resp.status_code, f.id, f.title))
     return True
+
 
 if __name__ == "__main__":
     main()
-
