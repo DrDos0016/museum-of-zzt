ae2a301b5116831947d9c08c7e7b558002bcc918
Dr. Dos <doctordos@gmail.com>
2021-10-11 15:29:06 -0700

    Example versions of python files with private settings

---
+++ museum/example-private.py
@@ -0,0 +1,14 @@
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+# https://docs.djangoproject.com/en/3.2/ref/settings/#std:setting-SECRET_KEY
+SECRET_KEY = ''
