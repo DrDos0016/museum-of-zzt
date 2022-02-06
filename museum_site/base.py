ed45016c7319d2df1f894ec17971d0d1c4d8abe1
Dr. Dos <doctordos@gmail.com>
2022-02-05 16:25:45 -0800

    Add specific error messages for mandatory subclass methods

--- museum_site/base.py
+++ museum_site/base.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 class BaseModel(models.Model):
     model_name = None
@@ -17,28 +18,28 @@ class BaseModel(models.Model):
     def preview_url(self):
         return "Preview url"
 
-    def as_block(self):
-        return "AB"
+    def as_block(self, view="detailed", *args, **kwargs):
+        return getattr(self, "as_{}_block".format(view))(*args, **kwargs)
 
-    def as_detailed_block(self):
-        return "AB"
+    def as_detailed_block(self, *args, **kwargs):
+        raise NotImplementedError('Subclasses must implement "as_detailed_block" method.')
 
-    def as_list_block(self):
-        return "X"
+    def as_list_block(self, *args, **kwargs):
+        raise NotImplementedError('Subclasses must implement "as_list_block" this method.')
 
-    def as_gallery_block(self):
-        return "X"
+    def as_gallery_block(self, *args, **kwargs):
+        raise NotImplementedError('Subclasses must implement "as_gallery_block" method.')
 
-    def table_header(self):
+    @mark_safe
+    def table_header(self, *args, **kwargs):
         row = ""
         for f in self.table_fields:
             row += "<th>{}</th>".format(f)
         return "<tr>" + row + "</tr>"
 
+
     def scrub(self):
-        return "X"
-
-
+        raise NotImplementedError('Subclasses must implement "scrub" method.')
 
 
     class Meta:
