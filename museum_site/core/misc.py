568bb8e0405ef20035631e16dbf431a2e851efc0
Dr. Dos <doctordos@gmail.com>
2022-05-24 17:22:00 -0700

    Ability to strip arguments when redirecting to new URLs

--- museum_site/core/misc.py
+++ museum_site/core/misc.py
@@ -3,12 +3,13 @@ from django.urls import reverse
 
 
 def legacy_redirect(request, name=None, *args, **kwargs):
+    # Strip arguments if they're no longer needed
+    if "strip" in kwargs:
+        for stripped_arg in kwargs["strip"]:
+            kwargs.pop(stripped_arg)
+        kwargs.pop("strip")
     url = reverse(name, args=args, kwargs=kwargs)
     if request.META["QUERY_STRING"]:
         url += "?" + request.META["QUERY_STRING"]
 
     return redirect(url, permanent=True)
-
-#django.pi:8000/article/categories/
-#http://django.pi:8000/article/categories/?param1=A&param2=B
-#django.pi:8000/article/608/page/2/closer-look-warlock-domain/
