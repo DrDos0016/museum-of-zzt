42f4a89971be55d4034c3de141e551cb28f69a6b
Dr. Dos <doctordos@gmail.com>
2022-05-26 14:46:53 -0700

    Remove unused values

--- museum_site/debug_views.py
+++ museum_site/debug_views.py
@@ -40,11 +40,11 @@ def debug(request, filename=None):
     data["available_views"] = ["detailed", "list", "gallery"]
     data["view"] = get_selected_view_format(request, data["available_views"])
 
-    data["wozzt_table_header"] = table_header(test_wozzt[0].table_fields)
-    data["review_table_header"] = table_header(test_reviews[0].table_fields)
-    data["zfile_table_header"] = table_header(test_zfiles[0].table_fields)
-    data["article_table_header"] = table_header(test_articles[0].table_fields)
-    data["series_table_header"] = table_header(test_series[0].table_fields)
+    #data["wozzt_table_header"] = table_header(test_wozzt[0].table_fields)
+    #data["review_table_header"] = table_header(test_reviews[0].table_fields)
+    #data["zfile_table_header"] = table_header(test_zfiles[0].table_fields)
+    #data["article_table_header"] = table_header(test_articles[0].table_fields)
+    #data["series_table_header"] = table_header(test_series[0].table_fields)
 
     data["wozzt"] = test_wozzt
     data["reviews"] = test_reviews
