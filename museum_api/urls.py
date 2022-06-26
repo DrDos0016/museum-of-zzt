71263a521f281f568bca3c1817522939f7353492
Dr. Dos <doctordos@gmail.com>
2022-06-26 15:27:46 -0700

    Move API v1 to a dedicated module

--- museum_api/urls.py
+++ museum_api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-import museum_api.endpoints
+#import museum_api.endpoints
+import museum_api.v1.endpoints
 
 
 urlpatterns = [
-    path("worlds-of-zzt/", museum_api.endpoints.worlds_of_zzt, name="api_wozzt"),
-    path("v1/get/file/", museum_api.endpoints.get_file, name="api_get_file"),
-    path("v1/get/random-file/", museum_api.endpoints.get_random_file, name="api_get_random_file"),
-    path("v1/help/", museum_api.endpoints.help, name="api_help"),
-    path("v1/search/files/", museum_api.endpoints.search_files, name="api_search_files"),
+    # Museum API - v2
+    # Museum API - v1
+    path("worlds-of-zzt/", museum_api.v1.endpoints.worlds_of_zzt, name="api_wozzt"),
+    path("v1/get/file/", museum_api.v1.endpoints.get_file, name="api_get_file"),
+    path("v1/get/random-file/", museum_api.v1.endpoints.get_random_file, name="api_get_random_file"),
+    path("v1/help/", museum_api.v1.endpoints.help, name="api_help"),
+    path("v1/search/files/", museum_api.v1.endpoints.search_files, name="api_search_files"),
 ]
