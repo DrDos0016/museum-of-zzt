81b2103389b95276c0295027cd34d6c1d36c6115
Dr. Dos <doctordos@gmail.com>
2022-06-26 15:30:47 -0700

    Additional work on Collections

--- museum_site/static/js/collection-manager.js
+++ museum_site/static/js/collection-manager.js
@@ -2,6 +2,21 @@
 
 $(document).ready(function (){
     $("#collection-add-button").click(add_item);
+    $("input[name=field_filter]").val("");
+    $("input[name=field_filter]").keyup(function (){
+        filter_file_list();
+    });
+
+    $("#field_filter_clear").click(function (){
+        $("input[name=field_filter]").val("");
+        filter_file_list();
+    });
+
+    $("input[name=associated_file]").click(function (){
+        $("#added-item-text").html("");
+        $("#id_associated_file li.selected").removeClass("selected");
+        $(this).parent().parent().addClass("selected")
+    });
 });
 
 function add_item()
@@ -16,7 +31,9 @@ function add_item()
 
     // Blank the fields
     var original_text = $("#collection-add-button").val();
+    var item_name = $("input[name=associated_file]:checked").parent().text().trim();
     $("#collection-add-button").prop("disabled", true);
+    $("#id_associated_file li.selected").removeClass("selected");
     $("#collection-add-button").val("Wait...");
     $("input[name=associated_file]:checked").prop("checked", false);
     $("textarea[name=collection_description]").val();
@@ -31,6 +48,7 @@ function add_item()
     ).done(
         function (){
             console.log("It worked!");
+            $("#added-item-text").html("Added " + item_name);
         }
     ).fail(
         function (){
@@ -71,4 +89,25 @@ function get_collection_contents()
             console.log("All done!");
         }
     );
+}
+
+function filter_file_list()
+{
+    var filter = $("input[name=field_filter]").val();
+    $("input[name=associated_file]").each(function (){
+        var radio = $(this);
+        var entry = $(this).parent().text().trim();
+
+        // Uncheck and hide non-matches
+        if (entry.indexOf(filter) == -1)
+        {
+            radio.prop("checked", "");
+            $(this).parent().parent().hide();
+        }
+        else
+        {
+            $(this).parent().parent().show();
+        }
+
+    });
 }
