d38ccf3b7b08b3a2bb63b88d72d26144cf0ffdda
Dr. Dos <doctordos@gmail.com>
2022-09-29 14:42:53 -0700

    Support for SZZT images in CLs

--- museum_site/static/js/debug-crop.js
+++ museum_site/static/js/debug-crop.js
@@ -8,11 +8,17 @@ R: <input name="bottom" value="">
 B: <input name="right" value="">
 (​r0p: <input id="crop-output">
 <label><input id="mcrop" type="checkbox" value="mcrop" style="height:20px"> M(r0p</label>
+<label><input id="szzt" type="checkbox" value="szzt" style="height:20px"> SZZT/label>
 <div style="display:none">
 Raw X/Y: <input name="raw-xy" value="-/-">
 Tile X/Y: <input name="tile-xy" value="-/-">
 </div>
 </div>`;
+
+var CHAR_WIDTH = 8;
+var CHAR_HEIGHT = 14;
+var H_RES = 80;
+var V_RES = 25;
 
 var click_coord = null;
 
@@ -26,6 +32,24 @@ $(document).ready(function (){
     $("#crop-controls input").keyup(function (e){ adjust(e) });
     $("#crop-controls input").keyup(preview);
     $("body").keyup(function (e){ reset(e) });
+    $("#szzt").click(function (){
+        if ($(this).prop("checked"))
+        {
+            CHAR_WIDTH = 16;
+            CHAR_HEIGHT = 16;
+            H_RES = 40;
+            V_RES = 25;
+            console.log("Super ZZT Mode");
+        }
+        else
+        {
+            CHAR_WIDTH = 8;
+            CHAR_HEIGHT = 14;
+            H_RES = 80;
+            V_RES = 25;
+            console.log("ZZT Mode");
+        }
+    });
 });
 
 
@@ -43,8 +67,8 @@ var crop = function (data){
     $("#active").html($(this).children().attr("alt"));
     $("input[name=top]").val(1);
     $("input[name=left]").val(1);
-    $("input[name=bottom]").val(80);
-    $("input[name=right]").val(25);
+    $("input[name=bottom]").val(H_RES);
+    $("input[name=right]").val(V_RES);
     $("input[name=top]").click();
     $("input[name=raw-xy]").val("0/0");
     $("input[name=tile-xy]").val("0/0");
@@ -73,10 +97,13 @@ var preview = function (){
     var b = parseInt($("input[name=bottom]").val()) - 1;
     var r = parseInt($("input[name=right]").val()) - 1;
 
-    var left = t * 8;
-    var top = l * 14;
-    var w = (b - t + 1) * 8;
-    var h = (r - l + 1) * 14;
+    var left = t * CHAR_WIDTH;
+    var top = l * CHAR_HEIGHT;
+    var w = (b - t + 1) * CHAR_WIDTH;
+    var h = (r - l + 1) * CHAR_HEIGHT;
+
+    console.log("WIDTH IS", w);
+    console.log("HEIGHT IS", h);
 
     $(".zzt-img.editing").children().css({"max-width": "none", "position": "relative", "left": -1 * left, "top": -1 * top});
     $(".zzt-img.editing").css({"width": w, "height":h});
@@ -92,8 +119,8 @@ function translate_click(data){
     var rect = $(".editing").children()[0].getBoundingClientRect();
     var raw_x = data.pageX - rect.left - document.querySelector("html").scrollLeft - 1;
     var raw_y = data.pageY - rect.top - document.querySelector("html").scrollTop - 1; // minus 1px for the left and top border
-    var tile_x = parseInt(raw_x / 8) + 1;
-    var tile_y = parseInt(raw_y / 14) + 1;
+    var tile_x = parseInt(raw_x / CHAR_WIDTH) + 1;
+    var tile_y = parseInt(raw_y / CHAR_HEIGHT) + 1;
     $("input[name=raw-xy]").val(raw_x + "/" + raw_y);
     $("input[name=tile-xy]").val(tile_x + "/" + tile_y);
 
@@ -121,8 +148,8 @@ function reset(e)
     click_coord = null;
     $("input[name=top]").val(1);
     $("input[name=left]").val(1);
-    $("input[name=bottom]").val(80);
-    $("input[name=right]").val(25);
+    $("input[name=bottom]").val(H_RES);
+    $("input[name=right]").val(V_RES);
     $("input[name=top]").click();
     $("input[name=raw-xy]").val("0/0");
     $("input[name=tile-xy]").val("0/0");
--- museum_site/templatetags/zzt_tags.py
+++ museum_site/templatetags/zzt_tags.py
@@ -233,6 +233,11 @@ class ZztScroll(template.Node):
 
 @register.simple_tag()
 def zzt_img(*args, **kwargs):
+    # Set up zzt/szzt specifics
+    CHAR_WIDTH = 16 if kwargs.get("szzt") else 8
+    CHAR_HEIGHT = 16 if kwargs.get("szzt") else 14
+    ENGINE = "szzt" if kwargs.get("szzt") else "zzt"
+
     # Combine basic args into the image path
     if len(args) == 1:
         source = args[0]
@@ -259,10 +264,10 @@ def zzt_img(*args, **kwargs):
         tl = (int(tl.split(",")[0]) - 1, int(tl.split(",")[1]) - 1)
         br = (int(br.split(",")[0]) - 1, int(br.split(",")[1]) - 1)
 
-        left = tl[0] * 8
-        top = tl[1] * 14
-        width = (br[0] - tl[0] + 1) * 8
-        height = (br[1] - tl[1] + 1) * 14
+        left = tl[0] * CHAR_WIDTH
+        top = tl[1] * CHAR_HEIGHT
+        width = (br[0] - tl[0] + 1) * CHAR_WIDTH
+        height = (br[1] - tl[1] + 1) * CHAR_HEIGHT
 
         # Generate cropped CSS for image
         img_css = "max-width:none;position:relative;left:-{}px;".format(left)
@@ -277,7 +282,7 @@ def zzt_img(*args, **kwargs):
         alt = os.path.splitext(os.path.basename(source))[0]
 
     # Crate the div/img tags
-    div = "div class='zzt-img{}' style='{}'".format(shorthand, css)
+    div = "div class='engine-{} zzt-img{}' style='{}'".format(ENGINE, shorthand, css)
 
     img = "img src='{}{}' class='{}' alt='{}' title='{}'".format(
         settings.STATIC_URL,
@@ -308,3 +313,7 @@ def zzt_img(*args, **kwargs):
     if caption:
         output += '<div class="img-caption">{}</div>'.format(caption)
     return mark_safe(output)
+
+@register.simple_tag()
+def szzt_img(*args, **kwargs):
+    return zzt_img(*args, **kwargs, szzt=True)
