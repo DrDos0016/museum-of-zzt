9a1641f9a4e38a0ae29a6bac036ff7754cd8273d
Dr. Dos <doctordos@gmail.com>
2022-10-12 11:05:15 -0700

    Move edit/delete upload links to top of page

--- museum_site/querysets/upload_querysets.py
+++ museum_site/querysets/upload_querysets.py
@@ -6,6 +6,10 @@ class Upload_Queryset(Base_Queryset):
     def unpublished_user_uploads(self, user_id):
         return self.filter(user_id=user_id, file__details=DETAIL_UPLOADED).order_by("-id")
 
+    def uploads_from_tokens(self, tokens):
+        """ Returns all uploads with provided tokens. This includes uploads which have been published. """
+        return self.filter(edit_token__in=tokens).order_by("-id")
+
     def from_token(self, token):
         qs = self.filter(edit_token=token)
         if qs:
