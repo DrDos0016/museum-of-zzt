d52e96c0d71850cbf8f6c93cb53e9eb213cecf6a
Dr. Dos <doctordos@gmail.com>
2023-01-02 16:25:41 -0800

    Adjust timestamps used for backups

--- tools/crons/museum-backup.py
+++ tools/crons/museum-backup.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from museum_site.constants import SITE_ROOT
 
@@ -15,51 +15,37 @@ def main():
     DUMP_SCRIPT = os.path.join(SITE_ROOT, "tools", "dump_database.py")
     DB_BACKUP = os.path.join(TEMP, "database.sql")
 
+    timestamp = datetime.now() + timedelta(days=-1)
+
     today = str(datetime.now())[:10]
 
     print("[1/3] Backing up database", datetime.now())
-    command = "{} {} {}".format(
-        PYTHON,
-        DUMP_SCRIPT,
-        DB_BACKUP
-    )
-    #print(command)
+    command = "{} {} {}".format(PYTHON, DUMP_SCRIPT, DB_BACKUP)
     os.system(command)
 
     print("Compressing...")
     tar_name = os.path.join(BACKUPS, today + "_database.tar.gz")
-    command = "tar -czf {} -C {} {}".format(
-        tar_name,
-        TEMP,
-        "database.sql"
-        )
-    #print(command)
+    command = "tar -czf {} -C {} {}".format(tar_name, TEMP, "database.sql")
     os.system(command)
     os.remove(os.path.join(TEMP, "database.sql"))
 
     print("[2/3] Backing up /zgames", datetime.now())
     print("Compressing...")
     tar_name = os.path.join(BACKUPS, today + "_zgames.tar.gz")
-    command = "tar --exclude='mass/*' -czf {} -C {} zgames".format(
-        tar_name,
-        SITE_ROOT,
-    )
-    #print(command)
+    command = "tar --exclude='mass/*' -czf {} -C {} zgames".format(tar_name, SITE_ROOT)
     os.system(command)
-
 
     print("[3/3] Backing up museum", datetime.now())
     print("Compressing...")
     tar_name = os.path.join(BACKUPS, today + "_museum.tar.gz")
-    command = "tar --exclude='.git' --exclude='backups/*' --exclude='log/*' --exclude='temp/*' --exclude='venv' --exclude='zgames' -czf {} -C {} museum-of-zzt".format(
-        tar_name,
-        PROJECTS,
-    )
-    print(command)
+    command = (
+        "tar --exclude='.git' --exclude='backups/*' --exclude='log/*' --exclude='temp/*' --exclude='venv' --exclude='zgames' -czf {} -C {} museum-of-zzt"
+    ).format(tar_name, PROJECTS,)
     os.system(command)
     print("DONE.", datetime.now())
 
     return True
+
 
 if __name__ == "__main__":
     main()
