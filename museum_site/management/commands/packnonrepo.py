68f6238cf37ae3a19a809e3aaa196e53b2f2cc07
Dr. Dos <doctordos@gmail.com>
2023-05-22 14:43:59 -0700

    Add packnonrepo command to django's management system

---
+++ museum_site/management/commands/packnonrepo.py
@@ -0,0 +1,39 @@
+import os
+import tarfile
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+#from museum_site.constansts import ZGAMES_BASE_PATH
+
+
+class Command(BaseCommand):
+    #https://docs.djangoproject.com/en/4.2/howto/custom-management-commands/
+    help = "Compresses defined non-repository content [zgames|articles|data|<app's_static_dir>]."
+
+    def add_arguments(self, parser):
+        parser.add_argument("items", nargs="+",)
+
+    def handle(self, *args, **options):
+        print(settings.BASE_DIR)
+        for item in options["items"]:
+            print("Compressing", item)
+            tar = tarfile.open(item + ".tar.gz", "w:gz")
+
+            # Specific Content
+            if item == "zgames":
+                tar_path = item
+                item_path = os.path.join(settings.BASE_DIR, tar_path)
+            elif item == "articles":
+                tar_path = os.path.join("museum_site", "static", "articles")
+                item_path = os.path.join(settings.BASE_DIR, tar_path)
+            elif item == "data":
+                tar_path = os.path.join("museum_site", "static", "data")
+                item_path = os.path.join(settings.BASE_DIR, tar_path)
+            else:
+                # Compress /static/ directory
+                tar_path = os.path.join(item, "static", item)
+                item_path = os.path.join(settings.BASE_DIR, tar_path)
+            tar.add(item_path, tar_path)
+
+            self.stdout.write(self.style.SUCCESS("Success"))
