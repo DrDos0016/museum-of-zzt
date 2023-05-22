fb129a362b6eb188950bbb29da1e1f41c57726bd
Dr. Dos <doctordos@gmail.com>
2023-05-22 15:37:25 -0700

    Add support for starting PK and delay

--- tools/archive_link_test.py
+++ tools/archive_link_test.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import sys
+import time
 import requests
 
 import django
@@ -12,12 +13,22 @@ from museum_site.models import File  # noqa: E402
 
 def main():
     print("Checks all files for valid archive.org links")
-    input("Press enter to continue")
-    for f in File.objects.all().order_by("id"):
+    starting_pk = input("Starting PK (def. 0): ")
+    delay = input("Delay between hits in seconds (def. 5s): ")
+
+    starting_pk = 0 if starting_pk == "" else int(starting_pk)
+    delay = 5 if delay == "" else int(delay)
+
+
+    for f in File.objects.filter(pk__gte=starting_pk).order_by("id"):
         if f.archive_name:
             resp = requests.head("https://archive.org/embed/" + f.archive_name)
             if resp.status_code != 200:
                 print("{} #{} `{}`".format(resp.status_code, f.id, f.title))
+            if delay:
+                time.sleep(delay)
+        if f.pk % 100 == 0:
+            print(f.pk)
     return True
 
 
