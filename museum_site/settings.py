f5748f3433b60d2d17f9fbea570e206b01729cc1
Dr. Dos <doctordos@gmail.com>
2023-05-22 15:43:58 -0700

    Let Python set default SMTP port

--- museum_site/settings.py
+++ museum_site/settings.py
@@ -37,7 +37,7 @@ NEW_REVIEW_WEBHOOK_URL = os.environ.get("MOZ_NEW_REVIEW_WEBHOOK_URL", "-UNDEFINE
 
 # SMTP
 SMTP_HOST = os.environ.get("MOZ_SMTP_HOST", "localhost")
-SMTP_PORT = os.environ.get("MOZ_SMTP_PORT", 25)
+SMTP_PORT = os.environ.get("MOZ_SMTP_PORT", 0)
 SMTP_AUTH_USER = os.environ.get("MOZ_SMTP_AUTH_USER", "-UNDEFINED-")
 SMTP_AUTH_PASS = os.environ.get("MOZ_SMTP_AUTH_PASS", "-UNDEFINED-")
 
