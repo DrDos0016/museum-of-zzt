832ee3a6f45ec9759aea4985cedeed5af9abe2a1
Dr. Dos <doctordos@gmail.com>
2023-07-07 16:57:56 -0700

    Formatting

--- museum_site/feeds.py
+++ museum_site/feeds.py
@@ -7,6 +7,7 @@ from django.template.defaultfilters import slugify
 
 from museum_site.models import Article, File, Review
 
+
 class Base_Feed(Feed):
     def item_title(self, item):
         return item.title
@@ -16,7 +17,6 @@ class Base_Feed(Feed):
 
     def item_link(self, item):
         return item.url()
-
 
 
 class Latest_Articles_Feed(Base_Feed):
@@ -36,12 +36,14 @@ class Latest_Articles_Feed(Base_Feed):
 
 class Upcoming_Articles_Feed(Latest_Articles_Feed):
     title = "Museum of ZZT - Latest/Upcoming Articles RSS"
+
     def items(self):
         return Article.objects.published_or_upcoming().order_by("-id")[:25]
 
 
 class Unpublished_Articles_Feed(Latest_Articles_Feed):
     title = "Museum of ZZT - Latest/Upcoming/Unpublished Articles RSS"
+
     def items(self):
         return Article.objects.published_or_upcoming_or_unpublished().order_by("-id")[:25]
 
