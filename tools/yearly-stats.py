f093f670fa3fae4ba03e756241d771721394e253
Dr. Dos <doctordos@gmail.com>
2024-01-20 11:59:27 -0800

    Vertically center nav action links. Updates for end of year stats script

--- tools/yearly-stats.py
+++ tools/yearly-stats.py
@@ -15,11 +15,18 @@ from museum_site.models import *  # noqa: E402
 from museum_site.constants import *  # noqa: E402
 from museum_site.core.detail_identifiers import *  #noqa: E402
 
+LAST_YEAR = "2023"
+THIS_YEAR = "2024"
+LAST_YEAR_START = "{}-01-01".format(LAST_YEAR)
+LAST_YEAR_END = "{}-12-31".format(LAST_YEAR)
+THIS_YEAR_START = "{}-01-01".format(THIS_YEAR)
+THIS_YEAR_END = "{}-12-31".format(THIS_YEAR)
+
 def main():
     if len(sys.argv) == 2:
         year = sys.argv[-1]
     else:
-        year = "2022"
+        year = LAST_YEAR  # The concluded year
     print("Year:", year)
     start_date = year + "-01-01 00:00:00Z"
     end_date = year + "-12-31 23:59:59Z"
@@ -31,41 +38,10 @@ def main():
 
     print("-" * 60)
     # Uploads by release date year
-    years = {
-        "None": 0,
-        "1991": 0,
-        "1992": 0,
-        "1993": 0,
-        "1994": 0,
-        "1995": 0,
-        "1996": 0,
-        "1997": 0,
-        "1998": 0,
-        "1999": 0,
-        "2000": 0,
-        "2001": 0,
-        "2002": 0,
-        "2003": 0,
-        "2004": 0,
-        "2005": 0,
-        "2006": 0,
-        "2007": 0,
-        "2008": 0,
-        "2009": 0,
-        "2010": 0,
-        "2011": 0,
-        "2012": 0,
-        "2013": 0,
-        "2014": 0,
-        "2015": 0,
-        "2016": 0,
-        "2017": 0,
-        "2018": 0,
-        "2019": 0,
-        "2020": 0,
-        "2021": 0,
-        "2022": 0,
-    }
+    years = {"None": 0}
+    for x in range(1991, int(LAST_YEAR) + 1):
+        years[str(x)] = 0
+
     for u in qs:
         zf = File.objects.filter(upload__id=u.pk).first()
         if not zf:
@@ -79,50 +55,61 @@ def main():
     print("-" * 60)
     # Uploads by platform
     names = {
+        10:"Modified Graphics",
         15:"ZZT",
         13:"Super ZZT",
         16:"ZIG",
+        21:"ZZT Board",
         27:"Clone",
-        #33:"Program",
+        28:"Source Code",
+        33:"Program",
         35:"Rom",
         37:"Weave ZZT",
+        31:"Image",
+        29:"Text",
+        25:"ZZT High Score",
+        39:"New Find",
+        34:"Compressed",
+        30:"HTML",
     }
-    lookups = [DETAIL_ZZT, DETAIL_SZZT, DETAIL_ZIG, DETAIL_CLONE_WORLD, DETAIL_WEAVE, DETAIL_ROM]
-    details = {
-        DETAIL_ZZT: 0,
-        DETAIL_SZZT: 0,
-        DETAIL_ZIG: 0,
-        DETAIL_CLONE_WORLD: 0,
-        #DETAIL_PROGRAM: 0,
-        DETAIL_ROM: 0,
-        DETAIL_WEAVE: 0,
-    }
+    details = {}
     for u in qs:
+        added_pk = False
         zf = File.objects.filter(upload__id=u.pk).first()
         if not zf:
             continue
+        #print(zf)
         f_details = zf.details.all().values_list("id", flat=True)
+        if not f_details:
+            print("NO DETAILS?", zf)
         for l in f_details:
-            if l in lookups:
+            if details.get(l) is not None:
                 details[l] = details[l] + 1
+            else:
+                details[l] = 1
 
+    print("Scanned", len(qs), "files")
     print("Platform totals:")
     for d in details.keys():
         print(names[d], details[d])
 
     print("-" * 60)
+
+
     # Uploads by Author
-    """
-    author_stats = {}
-    author_stats_this_year = {}
-    for u in qs:
-        authors = u.file.author.split("/")
+    author_stats = {}  # Any author that had something uploaded in <finished_year>
+    author_stats_this_year = {}  # Any author that had something with a release year of <finished_year>
+    for u in qs:  # All uploads for the year
+        zf = File.objects.filter(upload=u).first()
+        if not zf:
+            continue
+        authors = zf.authors.all()
         for a in authors:
             author_stats[a] = author_stats.get(a, 0) + 1
-            if u.file.release_date and str(u.file.release_date) >= start_date:
+            if zf.release_date and str(zf.release_date) >= start_date:
                 author_stats_this_year[a] = author_stats_this_year.get(a, 0) + 1
-    """
-    qs = File.objects.filter(publish_date__gte="2022-01-01", publish_date__lt="2023-01-01")
+
+    qs = File.objects.filter(publish_date__gte=LAST_YEAR_START, publish_date__lte=THIS_YEAR_START)
     authors = {}
     for zf in qs:
         for author in zf.authors.all():
@@ -130,16 +117,25 @@ def main():
                 authors[author.title] += 1
             else:
                 authors[author.title] = 1
-    print("TODO TODO TODO Sorry future me I didn't finish rewriting this TODO TODO TODO")
-    print("TODO TODO TODO Sorry future me I didn't finish rewriting this TODO TODO TODO")
-    print("TODO TODO TODO Sorry future me I didn't finish rewriting this TODO TODO TODO")
-    print("TODO TODO TODO Sorry future me I didn't finish rewriting this TODO TODO TODO")
-    print("TODO TODO TODO Sorry future me I didn't finish rewriting this TODO TODO TODO")
+
+    """
+    print(author_stats)
+    print("="*80)
+    print(author_stats_this_year)
+    print("="*80)
+    print(authors)
+    """
+    for k, v in author_stats.items():
+        print(v, k.title.replace(" ", "_"))
+    for k, v in authors.items():
+        print(v, k.replace(" ", "_"))
+
 
 
     print("ALL UPLOADS ================================")
     for a in author_stats.keys():
         if author_stats[a] > 2:
+            print(a)
             print(a + ";" + str(author_stats[a]))
     print(len(author_stats), "total authors")
 
