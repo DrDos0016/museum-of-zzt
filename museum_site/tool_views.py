71247630a6573faa3c5d6d5a59291d5b05f0c921
Dr. Dos <doctordos@gmail.com>
2025-03-27 18:23:29 -0700

    Show CSS variable names in color audit tool

--- museum_site/tool_views.py
+++ museum_site/tool_views.py
@@ -101,38 +101,46 @@ def audit(request, target, return_target_dict=False):
 @staff_member_required
 def audit_colors(request):
     data = {"title": "DEBUG COLORS", "stylesheets": {}, "variables": {}}
+    
+    data["solarized"] = [
+            "#002B36", "#073642", "#586E75", "#657B83",
+            "#839496", "#93A1A1", "#EEE8D5", "#FDF6E3",
+            "#B58900", "#CB4B16", "#DC322F", "#D33682",
+            "#6C71C4", "#268BD2", "#2AA198", "#859900",
+    ]
+    data["ega"] = [
+        "#000", "#00A", "#0A0", "#0AA",
+        "#A00", "#A0A", "#A50", "#AAA",
+        "#555", "#55F", "#5F5", "#5FF",
+        "#F55", "#F5F", "#FF5", "#FFF",
+    ]
 
     for full_path in glob.glob(os.path.join(STATIC_PATH, "css", "*.css")):
         stylesheet = os.path.basename(full_path)
         data["stylesheets"][stylesheet] = []
         data["variables"][stylesheet] = []
-        data["solarized"] = [
-            "#002B36", "#073642", "#586E75", "#657B83",
-            "#839496", "#93A1A1", "#EEE8D5", "#FDF6E3",
-            "#B58900", "#CB4B16", "#DC322F", "#D33682",
-            "#6C71C4", "#268BD2", "#2AA198", "#859900",
-        ]
-        data["ega"] = [
-            "#000", "#00A", "#0A0", "#0AA",
-            "#A00", "#A0A", "#A50", "#AAA",
-            "#555", "#55F", "#5F5", "#5FF",
-            "#F55", "#F5F", "#FF5", "#FFF",
-        ]
-
+        seen_var_names = []
         with open(full_path) as fh:
             for line in fh.readlines():
+                if not line.startswith(" "):
+                    continue
                 matches = re.findall("#(?:[0-9a-fA-F]{3}){1,2}", line)
                 if line.strip().startswith("--"):
                     for m in matches:
                         if m not in data["variables"][stylesheet]:
-                            data["variables"][stylesheet].append(m)
+                            foo = {"color": m, "name": line.split(":")[0]}
+                            if foo["name"] in seen_var_names:
+                                data["variables"][stylesheet].append({"special": True})
+                                seen_var_names = []
+                            data["variables"][stylesheet].append(foo)
+                            seen_var_names.append(foo["name"])
                 else:
                     for m in matches:
                         if m not in data["stylesheets"][stylesheet]:
                             data["stylesheets"][stylesheet].append(m)
 
-            data["stylesheets"][stylesheet].sort()
-            data["variables"][stylesheet].sort()
+            #data["stylesheets"][stylesheet].sort()
+            #data["variables"][stylesheet].sort()
 
     return render(request, "museum_site/tools/audit-colors.html", data)
 
